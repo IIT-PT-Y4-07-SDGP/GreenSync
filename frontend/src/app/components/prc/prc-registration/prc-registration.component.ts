c3729d346c010b92969bd5b81bdfffb30e82a1a5
InhazDev <inhazfahimdev@gmail.com>
2024-03-01 22:28:16 +0530

    Frontend code structure

---
+++ frontend/src/app/components/prc/prc-registration/prc-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prc-registration',
+  templateUrl: './prc-registration.component.html',
+  styleUrls: ['./prc-registration.component.scss']
+})
+export class PrcRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
