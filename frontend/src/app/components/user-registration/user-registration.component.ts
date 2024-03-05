608822dadeeb1264361c8a152c9c5110eae77d33
InhazDev <inhazfahimdev@gmail.com>
2024-03-05 20:32:04 +0530

    data validation done

--- frontend/src/app/components/user-registration/user-registration.component.ts
+++ frontend/src/app/components/user-registration/user-registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-registration',
@@ -8,15 +8,24 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class UserRegistrationComponent implements OnInit {
   selectedProfilePicture!: string;
-  options: FormGroup;
+  userRegFormGroup: FormGroup;
   
-  constructor(fb: FormBuilder) { 
-    this.options = fb.group({
+  constructor(private fb: FormBuilder) { 
+    this.userRegFormGroup = fb.group({
       hideRequired: false,
       floatLabel: 'auto',
   });}
 
   ngOnInit(): void {
+    this.userRegFormGroup = this.fb.group({
+      firstName:['', Validators.required],
+      lastName:['', Validators.required],
+      email:['', [Validators.required , Validators.email]],
+      username:['', Validators.required],
+      phoneNumber:['', [Validators.required, Validators.pattern(/^\d{11}$/)]],
+      password:['', [Validators.required, passwordValidator()]],
+      passwordConfirmation:['', [Validators.required, confirmPasswordValidator('password')]]
+    })
     this.selectedProfilePicture="app/assets/default-male-profile-picture.jpg"
     const fileInput = document.getElementById('fileInput') as HTMLInputElement;
     const component = this;
@@ -65,4 +74,39 @@ export class UserRegistrationComponent implements OnInit {
       reader.readAsDataURL(file);
     }
   }
+
+  onRegister(){
+    if(this.userRegFormGroup.valid){
+      console.log("hola");
+    }
+  }
+}
+
+export function passwordValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const value: string = control.value;
+
+    // Password should be minimum 8 characters long
+    if (value.length < 8) {
+      return { 'minlength': true };
+    }
+
+    // Password should contain at least one special character, one lowercase letter, one uppercase letter, and one number
+    const regex = /^(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    if (!regex.test(value)) {
+      return { 'invalidPassword': true };
+    }
+
+    return null;
+  };
+}
+
+export function confirmPasswordValidator(passwordControlName: string): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const password = control.root.get(passwordControlName)?.value;
+    const confirmPassword = control.value;
+
+    // Check if passwords match
+    return password === confirmPassword ? null : { 'passwordMismatch': true };
+  };
 }
