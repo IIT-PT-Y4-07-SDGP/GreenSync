7413edb043dc408d760c9fa48529098eac2f8277
InhazDev <inhazfahimdev@gmail.com>
2024-03-07 19:03:47 +0530

    generated jwt token for prc

--- backend/controllers/PRCController.js
+++ backend/controllers/PRCController.js
@@ -1,20 +1,19 @@
 // Importing the service class
 const PRCService = require("../Services/PRCService");
-// const AuthService = require("../Services/authService");
+const AuthService = require("../Services/authService");
 // creating instances for service class
 const prc = new PRCService();
-// const authService = new AuthService();
+const authService = new AuthService();
 
 class PRCController{
     // prc Registration
     async PRCRegistration (req,res) {
         try{
             // Validate the prc data and add prc to database
-            await prc.PRCRegister(req.body);
+            const newPRC = await prc.PRCRegister(req.body);
             // get JWT token and
-            // const token = authService.generateJWTToken(newUser.account.username, newUser.id, newUser.userRole);
-            // return res.status(200).json({token:token});
-            return res.status(200).json({message:"PRC Controller Works"});
+            const token = authService.generateJWTToken(newPRC.username, newPRC.userRole);
+            return res.status(200).json({token:token});
         }
         catch(error){
             res.status(400).json({error:error.message})
