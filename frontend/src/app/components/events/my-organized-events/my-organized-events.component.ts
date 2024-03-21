e5f3702e9f533d3da603975e8d67de5beecd3d6a
InhazDev <inhazfahimdev@gmail.com>
2024-03-21 21:58:11 +0530

    fixed errors

--- frontend/src/app/components/events/my-organized-events/my-organized-events.component.ts
+++ frontend/src/app/components/events/my-organized-events/my-organized-events.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { DatePipe } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { OrganizeEventComponent } from 'src/app/components/events/organize-event/organize-event.component';
-import { environment } from 'src/environments/environment';
 import { Event } from 'src/app/interfaces/event';
+import { EventService } from '../../../services/event.service';
 
 
 @Component({
@@ -17,8 +16,11 @@ export class MyOrganizedEventsComponent implements OnInit {
   showButton: boolean = true; // Flag to control the visibility of the button
   eventResponse: any; // Property to hold the response from the server
   public events: Event[] = [];
-  apiUrl = environment.apiUrl;
-  constructor(public dialog: MatDialog, private http: HttpClient, private datePipe: DatePipe) { }
+  constructor(
+    public dialog: MatDialog, 
+    private http: HttpClient,
+    private eventService: EventService
+  ) { }
 
   public imagePaths: string[] = [
     'app/assets/event-list-img-1.jpg',
@@ -40,24 +42,7 @@ export class MyOrganizedEventsComponent implements OnInit {
 
 
   fetchOrganizingEvents(eventOrganizer: string): void {
-    // Make an HTTP request to fetch events from your server
-    this.http.get<Event[]>(`${this.apiUrl}/api/events/get-my-organizing-events?organizerId=${eventOrganizer}`).subscribe(events => {
-      // Assign the retrieved events to the component property
-      this.events = events.map(event => ({
-        _id: event._id,
-        eventName: event.eventName,
-        eventTime: event.eventTime,
-        eventLocation: event.eventLocation,
-        eventDescription: event.eventDescription,
-        eventOrganizer: event.eventOrganizer,
-        eventParticipant: event.eventParticipant,
-        eventToken: event.eventToken,
-        eventStatus: event.eventStatus,
-        createdAt: event.createdAt,
-        updatedAt: event.updatedAt,
-        __v: event.__v
-      }));
-    });
+    this.eventService.getOrganizingEvents(eventOrganizer)
   }
 
   formatDate(dateTime: string): string {
@@ -96,24 +81,20 @@ export class MyOrganizedEventsComponent implements OnInit {
 
   onClickStartEvents(event: string) {
     const eventId = event.toString();
-    const url = `${this.apiUrl}/api/events/start-event/${eventId}`;
-    this.http.post(url, {}).subscribe(
-      (response) => {
-      console.log('Event started successfully:', response);
-      this.eventResponse = response; // Save the response in the property
+    this.eventService.startEvent(eventId).subscribe({
+      next: response => {
+        console.log('Event started successfully:', response);
+        this.eventResponse = response; // Save the response in the property
         this.showButton = false;
-    },
-    (error) => {
-      console.error('Error starting event:', error);
-      // Handle any error that occurred while starting the event
-    }
-  );
+      }, 
+      error: err => {
+        console.error('Error starting event:', err);
+        // Handle any error that occurred while starting the event
+      }
+    });
   }
 
   isEventStarted(event: any){
     return event.eventStatus === 'Started' && /^\d{6}$/.test(event.eventToken);
-
   }
-  
-
 }
