b8887c79b433b7dc3869e48819e06e28db91b1ed
InhazDev <inhazfahimdev@gmail.com>
2024-03-21 23:54:35 +0530

    fixed events fetching issues

--- frontend/src/app/services/event.service.ts
+++ frontend/src/app/services/event.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Event } from 'src/app/interfaces/event';
+import { EventDetails } from 'src/app/interfaces/event';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,7 @@ import { Event } from 'src/app/interfaces/event';
 export class EventService {
   public headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   public apiUrl = environment.apiUrl;
-  public events: Event[] = [];
+  public events: EventDetails[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -18,28 +18,14 @@ export class EventService {
     return this.http.post<any>(`${this.apiUrl}/events/organize-event`, data, { headers: this.headers });
   }
 
-  getOrganizingEvents(eventOrganizer: string){
-    // Make an HTTP request to fetch events from your server
-    this.http.get<Event[]>(`${this.apiUrl}/events/get-my-organizing-events?organizerId=${eventOrganizer}`)
-      .subscribe(events => {
-        // Assign the retrieved events to the component property
-        this.events = events.map(event => ({
-          _id: event._id,
-          eventName: event.eventName,
-          eventTime: event.eventTime,
-          eventLocation: event.eventLocation,
-          eventDescription: event.eventDescription,
-          eventOrganizer: event.eventOrganizer,
-          eventParticipant: event.eventParticipant,
-          eventToken: event.eventToken,
-          eventStatus: event.eventStatus,
-          createdAt: event.createdAt,
-          updatedAt: event.updatedAt,
-          __v: event.__v
-        }));
-    });
+  getOrganizingEvents(eventOrganizer: string) {
+    return this.http.get<EventDetails[]>(`${this.apiUrl}/events/get-my-organizing-events?organizerId=${eventOrganizer}`)
   }
-  startEvent(eventId:string, ){
+  startEvent(eventId: string,) {
     return this.http.post(`${this.apiUrl}/events/start-event/${eventId}`, {})
-  } 
+  }
+
+  getAllEvents(){
+    return this.http.get<EventDetails[]>(`${this.apiUrl}/events/get-events`)
+  }
 }
