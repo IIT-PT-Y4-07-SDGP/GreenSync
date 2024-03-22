642f9135c7e5b139b1fa474f43186ca2d7ae40fb
Lakshitha-Gamage <lakshitha.2019318@iit.ac.lk>
2024-03-22 21:15:32 +0530

    GS-111-feature/prc/change-driver-for-pickups

--- backend/controllers/PRCController.js
+++ backend/controllers/PRCController.js
@@ -5,18 +5,47 @@ const AuthService = require("../Services/authService");
 const prc = new PRCService();
 const authService = new AuthService();
 
-class PRCController{
-    // prc Registration
-    static async PRCRegistration (req,res) {
-        try{
-            // Validate the prc data and add prc to database
-            const newPRC = await prc.PRCRegister(req.body, res);
-            return res.status(200).json({newPRC});
-        }
-        catch(error){
-            res.status(400).json({error:error.message})
-        }
+class PRCController {
+  // prc Registration
+  static async PRCRegistration(req, res) {
+    try {
+      // Validate the prc data and add prc to database
+      const newPRC = await prc.PRCRegister(req.body, res);
+      return res.status(200).json({ newPRC });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
+  }
+
+  /**
+   * Assigne pickups by truck and dirivers
+   * @param {*} req
+   * @param {*} res
+   */
+  static async PRCAssignePickups(req, res) {
+    try {
+      const assignedPickups = await prc.PRCAssignePickups(req, res);
+      return res.status(200).json({ assignedPickups });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
+  /**
+   * Change driver for pickup
+   * @param {*} req in request prcId as a param and req.body in driverIds
+   */
+  static async PRCChangeDriver(req, res) {
+    try {
+      const changedDriver = await prc.PRCChangeDriver(
+        req.params.prcId,
+        req.body
+      );
+      return res.status(200).json({ changedDriver });
+    } catch (error) {
+      res.status(403).json({ error: error.message });
+    }
+  }
 }
 
 // Export the controller
--- backend/routes/prc.js
+++ backend/routes/prc.js
@@ -1,9 +1,13 @@
 const PRCController = require("../controllers/PRCController");
-const prc = new PRCController(); 
+const prc = new PRCController();
 
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
 // router.post("/registration", prc.PRCRegistration);
+
+router.put("/assign-pickups/:prcId", PRCController.PRCAssignePickups);
+
+router.put("/change-driver/:prcId", PRCController.PRCChangeDriver);
 
 module.exports = router;
