80785d4ff18ea3dfa8a0196bae5b694cadb90115
Lakshitha-Gamage <lakshitha.2019318@iit.ac.lk>
2024-03-22 21:20:26 +0530

    GS-112-prc-driver-update-task

--- backend/routes/prc.js
+++ backend/routes/prc.js
@@ -1,9 +1,15 @@
 const PRCController = require("../controllers/PRCController");
-const prc = new PRCController(); 
+const prc = new PRCController();
 
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
 // router.post("/registration", prc.PRCRegistration);
+
+router.put("/assign-pickups/:prcId", PRCController.PRCAssignePickups);
+
+router.put("/change-driver/:prcId", PRCController.PRCChangeDriver);
+
+router.put("/change-driver-task/:prcId", PRCController.PRCDriverUpdateTask);
 
 module.exports = router;
