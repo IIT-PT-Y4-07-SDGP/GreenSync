45ef6deb1e60de8a0be80523978d136e255649a7
Lakshitha-Gamage <lakshitha.2019318@iit.ac.lk>
2024-03-22 21:27:43 +0530

    GS-113-feature/prc/request-for-on-demand-pickups

--- backend/routes/prc.js
+++ backend/routes/prc.js
@@ -6,4 +6,12 @@ const router = express.Router();
 
 // router.post("/registration", prc.PRCRegistration);
 
+router.put("/assign-pickups/:prcId", PRCController.PRCAssignePickups);
+
+router.put("/change-driver/:prcId", PRCController.PRCChangeDriver);
+
+router.put("/change-driver-task/:prcId", PRCController.PRCDriverUpdateTask);
+
+router.post("/request-demand-pickups", PRCController.PRCRequestDemandPickups);
+
 module.exports = router;
