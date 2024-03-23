ac1765bdcf8dee82f9f9c69077288f022bc05cd3
AadhilAN <aadhil.naleemdeen@gmail.com>
2024-03-23 14:45:02 +0530

    Code commit for organized event

--- frontend/src/app/pages/organized-event-page/organized-event-page.component.ts
+++ frontend/src/app/pages/organized-event-page/organized-event-page.component.ts
@@ -10,38 +10,46 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class OrganizedEventPageComponent implements OnInit {
   public events: EventDetails[] = [];
-  eventId: string = '';
+  eventId: string = '65fca7debb560277cb2dfd26';
   constructor(private eventServices: EventService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.eventId = params['eventId'];
-    });
+    this.fetchEvents(this.eventId);
+    // this.route.params.subscribe(params => {
+    //   this.eventId = params['eventId'];
+    //   console.log(this.eventId);
+    // });
   }
 
   fetchEvents(eventId: string): void {
       // Make an HTTP request to fetch events from your server
     this.eventServices.getEvent(eventId).subscribe({
-        next: events => {
-          // Assign the retrieved events to the component property
-          this.events = events.map(event => ({
-            _id: event._id,
-            eventName: event.eventName,
-            eventTime: event.eventTime,
-            eventLocation: event.eventLocation,
-            eventDescription: event.eventDescription,
-            eventOrganizer: event.eventOrganizer,
-            eventParticipant: event.eventParticipant,
-            eventToken: event.eventToken,
-            eventStatus: event.eventStatus,
-            createdAt: event.createdAt,
-            updatedAt: event.updatedAt,
-            __v: event.__v
-          }));
-        }, error: err => {
-          console.log(err);
-        }
-    });
+    next: event => {
+      // Check if the event object is not empty/null
+      if (event) {
+        // Since you're fetching a single event, you can directly assign it to this.events
+        this.events = [{
+          _id: event._id,
+          eventName: event.eventName,
+          eventTime: event.eventTime,
+          eventLocation: event.eventLocation,
+          eventDescription: event.eventDescription,
+          eventOrganizer: event.eventOrganizer,
+          eventParticipant: event.eventParticipant,
+          eventToken: event.eventToken,
+          eventStatus: event.eventStatus,
+          createdAt: event.createdAt,
+          updatedAt: event.updatedAt,
+          __v: event.__v
+        }];
+      } else {
+        console.log('No event found or invalid data returned.');
+      }
+    },
+    error: err => {
+      console.log(err);
+    }
+  });
   }
 
 
