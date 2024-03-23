2f3701baede211955306fcac22a215bd8ed46c8c
Lakshitha Gamage <94306527+Lakshitha-Gamage@users.noreply.github.com>
2024-03-23 15:01:08 +0530

    general public on demand pickup - UI (#82)

---
+++ frontend/src/app/pages/public/pickup/pickup.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PickupComponent } from './pickup.component';
+
+describe('PickupComponent', () => {
+  let component: PickupComponent;
+  let fixture: ComponentFixture<PickupComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PickupComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PickupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
