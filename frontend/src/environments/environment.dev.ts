be26fe94b89be74b6edd0045d4263f141c8868e7
Inhaz Fahim <77190303+InhazFahim@users.noreply.github.com>
2024-03-30 23:18:38 +0530

    Gs 161 configure api calls to different environments in frontend (#111)
    * Code commit
    
    * Removed unused import
    
    * Updated the correct API URL for environment files
    
    * Base API configured

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8475' // Required to include URL of the DEV Environment
+  apiUrl: 'https://backend-dev.greensync.com.lk'
 };
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:1000' // required to include URL of the production 
+  apiUrl: 'https://backend.greensync.com.lk' 
 };
