41a94a23e1818e2b41046c421c0e8c9ff186015a
InhazDev <inhazfahimdev@gmail.com>
2024-03-31 19:13:44 +0530

    [GS-116] event mgt delete events (#109)
    * Add deleteEvent method to EventsService and eventsController
    
    * Add authorization for event deletion

--- frontend/src/app/services/event.service.ts
+++ frontend/src/app/services/event.service.ts
@@ -36,28 +36,32 @@ export class EventService {
   getParticipateEvents(userID: string) {
     return this.http.get<ParticipateEventsResponse>(`${this.apiUrl}/events/get-participated-events?participantId=${userID}`)
   }
-
+  
   verifyToken(data: any) {
     return this.http.post<boolean>(`${this.apiUrl}/events/verify-event-token`, data, { headers: this.headers });
   }
-
+  
   startEvent(eventId: string,) {
     return this.http.post(`${this.apiUrl}/events/start-event/${eventId}`, {})
   }
-
+  
   getAllEvents(){
     return this.http.get<EventDetails[]>(`${this.apiUrl}/events/get-events`)
   }
-
+  
   registerUserToEvent(data: any): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/events/participate`, data, { headers: this.headers });
   }
-
+  
   getEvent(eventId: string){
     return this.http.get<EventDetails>(`${this.apiUrl}/events/get-event-total-registered?eventId=${eventId}`)
   }
-
+  
   completeEvent(data: any){
     return this.http.post(`${this.apiUrl}/events/end-event`, data, { headers: this.headers })
+  }
+  
+  deleteEvent(eventID: string, organizerID: string) {
+    return this.http.delete(`${this.apiUrl}/events/delete-event?eventID=${eventID}&organizerID=${organizerID}`);
   }
 }
