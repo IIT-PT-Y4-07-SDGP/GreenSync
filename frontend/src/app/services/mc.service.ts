87e0d6844d04ee55ad304268f929291278814b59
Inhaz Fahim <77190303+InhazFahim@users.noreply.github.com>
2024-04-05 12:30:15 +0530

    Fixed url overriding issue (#123)

--- frontend/src/app/services/mc.service.ts
+++ frontend/src/app/services/mc.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MC } from '../interfaces/MC';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 import { EnvironmentService } from './environment.service';
 
 @Injectable({
@@ -10,7 +9,7 @@ import { EnvironmentService } from './environment.service';
 })
 export class McService {
   headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-  apiUrl?:string;
+  apiUrl?:string|undefined|null;
 
   constructor(private http: HttpClient, private envService: EnvironmentService) { 
     this.apiUrl = this.envService.getBaseURL();
